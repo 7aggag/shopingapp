9c8367b5da6111b61a0d5e90ac30556c878030e5
haggagovic <m.haggagovic@gmail.com>
2019-10-27 15:30:23 -0700

    tableview

---
+++ shoping/shoping/servies/servies.swift
@@ -0,0 +1,9 @@
+//
+//  servies.swift
+//  shoping
+//
+//  Created by mohamed haggag on 10/27/19.
+//  Copyright © 2019 mohamed haggag. All rights reserved.
+//
+
+import Foundation
---
+++ shoping/shoping/view/categoriecellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  categoriecellTableViewCell.swift
+//  shoping
+//
+//  Created by mohamed haggag on 10/27/19.
+//  Copyright © 2019 mohamed haggag. All rights reserved.
+//
+
+import UIKit
+
+class categoriecellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
